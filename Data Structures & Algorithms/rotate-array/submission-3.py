33157bea2a7c972a7c15ae80383d01e2bf2760f3
Devansh  Singh <154344370+devanshsingh24@users.noreply.github.com>
2026-08-22 15:23:46 +0530

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n=len(nums)
+        k=k%n
+        nums[:]=nums[n-k:]+nums[:n-k]
+        
+        #k=k%len(nums)
+        #res=[]
+        #for j in range(len(nums)-k,len(nums)):
+        #    res.append(nums[j])
+        #for i in range(len(nums)-k):
+        #    res.append(nums[i])
+        #nums[:] = res
